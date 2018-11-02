89e376181995aecdf7ce60e6648ffd5fe8bca07e
huangalin <huangalin@laiyifen.com>
2018-11-02 21:58:11 +0800

    spring config

--- spring-clound/clound-config-client/src/main/java/com/hal/spring/clound/config/client/CloundConfigClientApplication.java
+++ spring-clound/clound-config-client/src/main/java/com/hal/spring/clound/config/client/CloundConfigClientApplication.java
@@ -1,12 +1,24 @@
 package com.hal.spring.clound.config.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CloundConfigClientApplication {
+
+    @Value("${foo}")
+    private String foo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloundConfigClientApplication.class, args);
 	}
+
+    @RequestMapping(value = "/foo")
+    public String getFoo(){
+        return foo;
+    }
 }
--- spring-clound/clound-config-server/src/main/java/com/hal/spring/clound/config/server/CloundConfigServerApplication.java
+++ spring-clound/clound-config-server/src/main/java/com/hal/spring/clound/config/server/CloundConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.hal.spring.clound.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CloundConfigServerApplication {
 
 	public static void main(String[] args) {
