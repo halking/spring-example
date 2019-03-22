afe44bdcafbde34e0a280195d4ec7ec6f7001301
Philippe HAN <philippe-hh.han@richemont.com>
2019-03-22 18:39:02 +0800

    Add excelUtil

--- all-sample/src/main/java/com/hal/sample/util/ExcelUtil.java
+++ all-sample/src/main/java/com/hal/sample/util/ExcelUtil.java
@@ -1,17 +1,21 @@
 package com.hal.sample.util;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static org.apache.commons.lang3.StringUtils.capitalize;
 import static org.apache.commons.lang3.StringUtils.endsWith;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.apache.commons.lang3.StringUtils.substringBefore;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -27,7 +31,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.util.ReflectionUtils;
 
 /**
  * Created by Steven.HUANG on 2018/12/25.
@@ -36,57 +39,57 @@ import org.springframework.util.ReflectionUtils;
 public class ExcelUtil {
 
   private final static String LESS_THAN_EQUAL_2003V = "xls";
+
   private final static String GREATER_THAN_EQUAL_2007V = "xlsx";
 
   /**
    * @param cell
    */
   private static Object getCellValue(Cell cell) {
-    if (cell == null ||
-      (cell.getCellTypeEnum() == CellType.STRING && isBlank(cell.getStringCellValue()))) {
-      return null;
-    }
-    CellType cellType = cell.getCellTypeEnum();
-    if (cellType == CellType.BLANK) {
-      return null;
-    } else if (cellType == CellType.BOOLEAN) {
-      return cell.getBooleanCellValue();
-    } else if (cellType == CellType.ERROR) {
-      return cell.getErrorCellValue();
-    } else if (cellType == CellType.FORMULA) {
-      try {
+    Object object = "";
+    try {
+      if (cell == null || (cell.getCellType() == CellType.STRING &&
+        isBlank(cell.getStringCellValue()))) {
+        return null;
+      }
+      CellType cellType = cell.getCellType();
+      if (cellType == CellType.BLANK) {
+        return null;
+      } else if (cellType == CellType.BOOLEAN) {
+        object = cell.getBooleanCellValue();
+      } else if (cellType == CellType.ERROR) {
+        object = cell.getErrorCellValue();
+      } else if (cellType == CellType.FORMULA) {
         if (HSSFDateUtil.isCellDateFormatted(cell)) {
-          return cell.getDateCellValue();
+          object = cell.getDateCellValue();
         } else {
-          return cell.getNumericCellValue();
+          object = cell.getNumericCellValue();
         }
-      } catch (IllegalStateException e) {
-        return cell.getRichStringCellValue();
-      }
-    } else if (cellType == CellType.NUMERIC) {
-      if (DateUtil.isCellDateFormatted(cell)) {
-        return cell.getDateCellValue();
+      } else if (cellType == CellType.NUMERIC) {
+        if (DateUtil.isCellDateFormatted(cell)) {
+          object = cell.getDateCellValue();
+        } else {
+          DecimalFormat df = new DecimalFormat("0");
+          object = df.format(cell.getNumericCellValue());
+        }
+      } else if (cellType == CellType.STRING) {
+        object = cell.getStringCellValue();
       } else {
-        return cell.getNumericCellValue();
+        return null;
       }
-    } else if (cellType == CellType.STRING) {
-      return cell.getStringCellValue();
-    } else {
-      return null;
+    } catch (Exception e) {
+      return object;
     }
+    return object;
   }
 
   public static List<List<Object>> readByRow(InputStream in, String fileName) throws IOException {
+    Workbook wb = getWorkbook(in, fileName);
+
     List<List<Object>> resultList = newArrayList();
-
-    Workbook work = getWorkbook(in, fileName);
-    if (null == work) {
-      throw new RuntimeException("create workbook failed...");
-    }
-
     //iterate the sheet in Workbook
-    for (int i = 0; i < work.getNumberOfSheets(); i++) {
-      Sheet sheet = work.getSheetAt(i);
+    for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+      Sheet sheet = wb.getSheetAt(i);
       if (sheet == null) {
         continue;
       }
@@ -106,6 +109,116 @@ public class ExcelUtil {
       }
     }
     return resultList;
+  }
+
+  public static <E> List<E> parseToObject(InputStream in, String fileName, Class<E> clazz)
+    throws Exception {
+
+    Workbook wb = getWorkbook(in, fileName);
+
+    List<E> resultList = newArrayList();
+
+    //iterate the sheet in Workbook
+    for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+      Sheet sheet = wb.getSheetAt(i);
+      if (sheet == null) {
+        continue;
+      }
+
+      Map<Integer, Field> fieldsMap = getTitle(sheet, clazz);
+      //iterate the row in sheet
+      for (int j = 1; j <= sheet.getLastRowNum(); j++) {
+        Row row = sheet.getRow(j);
+        if (row == null) {
+          continue;
+        }
+        //iterate column in the row
+        E entity = clazz.newInstance();
+        for (int y = row.getFirstCellNum(); y < row.getLastCellNum(); y++) {
+          Field field = fieldsMap.get(y);
+          if (field == null) {
+            continue;
+          }
+
+          Cell cell = row.getCell(y);
+          String value = dealCellType(getCellValue(cell));
+          if (isBlank(value)){
+            continue;
+          }
+
+          Class<?> fieldType = field.getType();
+          if (String.class == fieldType) {
+            field.set(entity, String.valueOf(value));
+          } else if ((Integer.TYPE == fieldType) || (Integer.class == fieldType)) {
+            field.set(entity, Integer.valueOf(value));
+          } else if ((Long.TYPE == fieldType) || (Long.class == fieldType)) {
+            field.set(entity, Long.valueOf(value));
+          } else if ((Float.TYPE == fieldType) || (Float.class == fieldType)) {
+            field.set(entity, Float.valueOf(value));
+          } else if ((Short.TYPE == fieldType) || (Short.class == fieldType)) {
+            field.set(entity, Short.valueOf(value));
+          } else if ((Double.TYPE == fieldType) || (Double.class == fieldType)) {
+            field.set(entity, Double.valueOf(value));
+          } else if (Character.TYPE == fieldType) {
+            if (value.length() > 0) {
+              field.set(entity, value.charAt(0));
+            }
+          } else if (BigDecimal.class == fieldType){
+            field.set(entity, new BigDecimal(value));
+          }
+        }
+        resultList.add(entity);
+      }
+    }
+    return resultList;
+  }
+
+
+  public static Map<Integer, Field> getTitle(Sheet sheet, Class<?> clazz) {
+    List<Field> fields = getMappedFiled(clazz, null);
+
+    Map<Integer, Field> fieldsMap = Maps.newHashMap();
+
+    Row rowHead = sheet.getRow(0);
+    Map<String, Integer> cellMap = new HashMap<>();
+    int cellNum = rowHead.getLastCellNum();
+    for (int i = 0; i < cellNum; i++) {
+      cellMap.put(rowHead.getCell(i).getStringCellValue(), i);
+    }
+
+    for (Field field : fields) {
+      if (field.isAnnotationPresent(ExcelField.class)) {
+        ExcelField attr = field.getAnnotation(ExcelField.class);
+        Integer col = cellMap.get(attr.title());
+        //validate excel template
+        if (col == null) {
+          throw new RuntimeException("Excel template is invalid...");
+        }
+        field.setAccessible(true);
+        fieldsMap.put(col, field);
+      }
+    }
+    return fieldsMap;
+  }
+
+  private static List<Field> getMappedFiled(Class clazz, List<Field> fields) {
+    if (fields == null) {
+      fields = Lists.newArrayList();
+    }
+
+    Field[] allFields = clazz.getDeclaredFields();
+
+    for (Field field : allFields) {
+      if (field.isAnnotationPresent(ExcelField.class)) {
+        fields.add(field);
+      }
+    }
+    if (clazz.getSuperclass() != null
+      && !clazz.getSuperclass().equals(Object.class)) {
+      getMappedFiled(clazz.getSuperclass(), fields);
+    }
+
+    return fields;
   }
 
   private static Workbook getWorkbook(InputStream inputStream, String fileName) throws IOException {
@@ -140,21 +253,25 @@ public class ExcelUtil {
   public static <T> void buildContent(OutputStream outputStream,
     String sheetName, Map<String, String> heads, Collection<T> datum)
     throws Exception {
-
     buildContent(outputStream, sheetName, heads.values(), sheet -> {
-      Iterator<T> iterator = datum.iterator();
-      int rowIndex = 0;
-      while (iterator.hasNext()) {
-        Row row = sheet.createRow(++rowIndex);
+      try {
+        Iterator<T> iterator = datum.iterator();
+        int rowIndex = 0;
+        while (iterator.hasNext()) {
+          Row row = sheet.createRow(++rowIndex);
 
-        T object = iterator.next();
-        for (String key : heads.keySet()) {
-          int colIndex = 0;
-          String getMethod = "get" + capitalize(key);
-          Method method = ReflectionUtils.findMethod(object.getClass(), getMethod);
-          Object value = ReflectionUtils.invokeMethod(method, object);
-          addCell(row, colIndex++, value);
+          T object = iterator.next();
+          for (String key : heads.keySet()) {
+            int colIndex = 0;
+            Field field = object.getClass().getField(key);
+            field.setAccessible(true);
+            Object value = field.get(object);
+            addCell(row, colIndex++, value);
+          }
         }
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        throw new RuntimeException(e.getMessage(), e);
       }
     });
 
@@ -162,6 +279,9 @@ public class ExcelUtil {
   }
 
   public static String dealCellType(Object object) {
+    if (object == null){
+      return null;
+    }
     String str = String.valueOf(object);
     if (endsWith(str, ".0")) {
       return substringBefore(str, ".0");
