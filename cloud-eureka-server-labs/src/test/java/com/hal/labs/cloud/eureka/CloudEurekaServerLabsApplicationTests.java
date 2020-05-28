77eede43b1ef68a1a51d218f453444588895f3f2
Steven HUANG <steven.huang@digibridge.cn>
2020-05-28 17:42:08 +0800

    1. 新增eureka server

---
+++ cloud-eureka-server-labs/src/test/java/com/hal/labs/cloud/eureka/CloudEurekaServerLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hal.labs.cloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaServerLabsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
