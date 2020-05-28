1b5c399a31a7eb85982272b72e8c5cea58398005
Steven HUANG <steven.huang@digibridge.cn>
2020-05-28 16:24:45 +0800

    1. 配置config server

---
+++ cloud-config-server-labs/src/main/java/com/hal/labs/config/server/PropertiesHandler.java
@@ -0,0 +1,11 @@
+package com.hal.labs.config.server;
+
+import org.springframework.boot.env.PropertiesPropertySourceLoader;
+
+/**
+ * @Author: Steven HUANG
+ * @Date: 2020/5/26
+ */
+public class PropertiesHandler extends PropertiesPropertySourceLoader {
+
+}
