c8c468b5217c47f70e977bb04e7f1839f1cb5d57
Steven HUANG <steven.huang@digibridge.cn>
2020-05-29 16:15:54 +0800

    1. 新增ribbon loadbalancer retry

--- cloud-feign-labs/src/main/java/com/hal/labs/cloud/feign/CloudFeignLabsApplication.java
+++ cloud-feign-labs/src/main/java/com/hal/labs/cloud/feign/CloudFeignLabsApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
-@RibbonClient(name = "feign-labs-ribbon", configuration = RibbonConfiguration.class)
+@EnableRetry
+@RibbonClient(name = "cloud-client-service", configuration = RibbonConfiguration.class)
 public class CloudFeignLabsApplication {
 
   public static void main(String[] args) {
